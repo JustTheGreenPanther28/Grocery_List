a3da90d474612613ab4728f9a7c00f8848131255
Vedant Maheshwari <vedantmaheshwari2006@gmail.com>
2026-08-02 16:01:36 +0530

    DONE

--- src/main/java/com/grocery/dto/LoginRequest.java
+++ src/main/java/com/grocery/dto/LoginRequest.java
@@ -6,14 +6,12 @@ public class LoginRequest {
 
     @NotBlank
     private String username;
-
     @NotBlank
     private String password;
-
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
